a9b93681f69179821198b477d3fdc3ab9e375a6d
Chris <4160557+chrisoro@users.noreply.github.com>
2018-01-21 15:02:26 +0100

    updated

--- Promoter/Promoter.py
+++ Promoter/Promoter.py
@@ -38,6 +38,7 @@ def setup_logging(name):
 
     fh = logging.FileHandler(os.path.join(logdir,time.strftime("%H%M%S")+"_"+name+"_success"))
     fh.setFormatter(formatter)
+    logger.setLevel(logging.INFO)
     suclogger.addHandler(fh)
 
 def get_depth():
@@ -46,14 +47,14 @@ def get_depth():
 def is_confirmed(api, bundlehash):
     try:
         txhashes = api.find_transactions([bundlehash])['hashes']
-    except Exception as err:
+    except:
         badbundles.append(bundlehash)
         logger.error(traceback.format_exc())
         raise
 
     try:
         bundlestates = api.get_latest_inclusion(txhashes)['states'].values()
-    except Exception as err:
+    except:
         badbundles.append(bundlehash)
         logger.error(traceback.format_exc())
         raise
@@ -63,11 +64,55 @@ def is_confirmed(api, bundlehash):
     else:
         return False
 
+def promote(api, tx):
+    try:
+        promotable = api.helpers.is_promotable(tx.hash)
+    except:
+        logger.error(traceback.format_exc())
+        raise
+
+    if (promotable):
+        try:
+            logger.info('promoting tx: %s', tx.hash)
+            api.promote_transaction(tx.hash, get_depth())
+            logger.info('promoted successfully')
+        except iota.adapter.BadApiResponse as err:
+            if('inconsistent tips pair selected' in err.context['response']['errors'][0]):
+                pass
+            else:
+                logger.error(traceback.format_exc())
+            raise
+        except:
+            logger.error(traceback.format_exc())
+            raise
+
+def reattach(api, tx):
+    try:
+        reattachable = api.is_reattachable([tx.address])['reattachable'][0]
+    except:
+        logger.error(traceback.format_exc())
+        raise
+
+    if (reattachable):
+        try:
+            logger.info('reattaching tx: %s', tx.hash)
+            api.replay_bundle(tx.hash, get_depth())
+            logger.info('reattached successfully')
+        except iota.adapter.BadApiResponse as err:
+            if('inconsistent tips pair selected' in err.context['response']['errors'][0]):
+                pass
+            else:
+                logger.error(traceback.format_exc())
+            raise
+        except:
+            logger.error(traceback.format_exc())
+            raise
+
 def autopromote(api):
     while(True):
         try:
             tips = api.get_tips()['hashes']
-        except Exception as err:
+        except:
             logger.error(traceback.format_exc())
             continue
 
@@ -77,7 +122,7 @@ def autopromote(api):
         random.shuffle(tips)
         try:
             trytes = api.get_trytes(tips[:chunksize])['trytes']
-        except Exception as err:
+        except:
             logger.error(traceback.format_exc())
             continue
 
@@ -85,39 +130,40 @@ def autopromote(api):
             tx = iota.Transaction.from_tryte_string(x)
             if (tx.bundle_hash not in badbundles):
                 try:
-                    if ((tx.value > 1000**3) and ((time.time()-tx.timestamp) < 30 * 60) and not is_confirmed(api, tx.bundle_hash)):
-                        promote(api, None, tx)
-                except Exception:
+                    if ((tx.value > 1000**2) and ((time.time()-tx.timestamp) < 30 * 60) and not is_confirmed(api, tx.bundle_hash)):
+                        spam(api, None, tx)
+                except:
                     pass
 
-def promote(api, txid, trans=None):
+def spam(api, txid, trans=None):
     if (txid is not None):
         try:
             trytes = api.get_trytes([iota.TransactionHash(iota.TryteString(txid.encode('ascii')))])['trytes'][0]
-        except Exception as err:
+        except:
             logger.error(traceback.format_exc())
             return
         inputtx = iota.Transaction.from_tryte_string(trytes)
     else:
         inputtx = trans
     
-    logger.info('start promoting tx (%smin): %s (%s)', round((time.time()-inputtx.timestamp)/60), inputtx.hash, inputtx.bundle_hash)
+    startTime = time.time()
+    logger.info('start promoting tx (%smin, %smi): %s (%s)', round((startTime-inputtx.timestamp)/60), round(inputtx.value/1000**2), inputtx.hash, inputtx.bundle_hash)
     
     count = 0
-    maxCount = 3
+    maxCount = 10
     while (True):
         try:
             confirmed = is_confirmed(api, inputtx.bundle_hash)
-        except Exception:
+        except:
             break
         if (confirmed):
             logger.info('bundle confirmed: %s', inputtx.bundle_hash)
-            suclogger(inputtx.bundle_hash)
+            suclogger('%smin - %smi: %s', round((time.time()-startTime)/60), round(inputtx.value/1000**2), inputtx.bundle_hash)
             break
 
         try:
             txhashes = api.find_transactions([inputtx.bundle_hash])['hashes']
-        except Exception as err:
+        except:
             logger.error(traceback.format_exc())
             continue
 
@@ -125,7 +171,7 @@ def promote(api, txid, trans=None):
         
         try:
             trytes = api.get_trytes(txhashes)['trytes']
-        except Exception as err:
+        except:
             logger.error(traceback.format_exc())
             continue
 
@@ -135,49 +181,15 @@ def promote(api, txid, trans=None):
             if (tx.is_tail):
                 if (count < maxCount):
                     try:
-                        promotable = api.helpers.is_promotable(tx.hash)
-                    except Exception as err:
-                        logger.error(traceback.format_exc())
+                        promote(api, tx)
+                    except:
                         continue
-
-                    if (promotable):
-                        try:
-                            logger.info('promoting tx: %s', tx.hash)
-                            api.promote_transaction(tx.hash, get_depth())
-                            logger.info('promoted successfully')
-                        except Exception as err:
-                            if('errors' in err.context):
-                                if('inconsistent tips pair selected' in err.context['errors'][0]):
-                                    pass
-                                else:
-                                    logger.error(traceback.format_exc())
-                            else:
-                                logger.error(traceback.format_exc())
                 else:
                     try:
-                        reattachable = api.is_reattachable([tx.address])['reattachable'][0]
-                    except Exception as err:
-                        logger.error(traceback.format_exc())
+                        reattach(api, tx)
+                        break
+                    except:
                         continue
-
-                    if (reattachable):
-                        try:
-                            logger.info('reattaching tx: %s', tx.hash)
-                            api.replay_bundle(tx.hash, get_depth())
-                            logger.info('reattached successfully')
-                            break
-                        except Exception as err:
-                            if('errors' in err.context):
-                                if('inconsistent tips pair selected' in err.context['errors'][0]):
-                                    pass
-                                elif ('has invalid signature' in err.context['errors'][0]):
-                                    badbundles.append(tx.bundle_hash)
-                                    logger.error(err)
-                                    return
-                                else:
-                                    logger.error(traceback.format_exc())
-                            else:
-                                logger.error(traceback.format_exc())
         
         if (count == maxCount):
             count = 0
@@ -185,11 +197,14 @@ def promote(api, txid, trans=None):
             count += 1
 
         logger.info('finished. %s more runs until reattach', maxCount-count)
+        
+        ''' disabled sleep for testing purposes
         sleeping = random.randint(0,2)
         while (sleeping > 0):
             logger.info('%s min sleep...', sleeping)
             time.sleep(60)
             sleeping -= 1
+        '''
 
 
 if __name__ == "__main__":
@@ -202,7 +217,7 @@ if __name__ == "__main__":
     if (args.tx is not None):
         setup_logging(args.tx)
         logger.info('------------------------Start------------------------')
-        promote(api, args.tx)
+        spam(api, args.tx)
         logger.info('------------------------Finish------------------------')
         
     else:
